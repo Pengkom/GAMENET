ecde3443522b36d779cec86b66886853936e3491
Will Coronel <will.coronel@benilde.edu.ph>
2023-11-05 00:19:32 +0800

    [Module 3] 2.6/3

---
+++ Module 3/Assets/Scripts/PlayerSetup.cs
@@ -0,0 +1,25 @@
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class PlayerSetup : MonoBehaviourPunCallbacks
+{
+    public Camera camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.camera = transform.Find("Camera").GetComponent<Camera>();
+        if (PhotonNetwork.CurrentRoom.CustomProperties.ContainsValue("rc"))
+        {
+            GetComponent<VehicleMovement>().enabled = photonView.IsMine;
+            camera.enabled = photonView.IsMine;
+        }
+        else if (PhotonNetwork.CurrentRoom.CustomProperties.ContainsValue("dr"))
+        { 
+            
+        }
+    }
+}
